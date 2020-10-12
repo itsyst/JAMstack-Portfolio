05f573aba9785057ced72187c17db4afb83c074f
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-10-12 12:33:31 +0200

    deleted recaptcha field

--- portfolio_frontend/src/pages/contact.js
+++ portfolio_frontend/src/pages/contact.js
@@ -34,9 +34,6 @@ const contact = () => {
                                 className="form-control"
                             ></textarea>
                         </div>
-                        <div className="form-recaptcha">
-                            <div data-netlify-recaptcha="true"></div>
-                        </div>
                         <button type="submit" className="submit-btn btn">
                             Kontakta mig
                         </button>
