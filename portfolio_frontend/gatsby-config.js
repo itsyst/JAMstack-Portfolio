c704fc9eccc67d40f0a81a76f7c0a5da8e1525b8
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-10-12 09:17:52 +0200

    created .env dev and .env prod

--- portfolio_frontend/gatsby-config.js
+++ portfolio_frontend/gatsby-config.js
@@ -1,11 +1,14 @@
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
     siteMetadata: {
         title: "khaled's Portfolio",
         description: "This is WebDev Portfolio Site",
         author: "Khaled El Hamzi",
         twitterUsername: "@KhaledElHamzi",
-        image: "/twitter-ing.png",
-        siteUrl: "https://khaled-tentan.netlify.com",
+        image: "/twitter-img.png",
+        siteUrl: "https://elhamzi.me",
     },
     plugins: [
         `gatsby-transformer-sharp`,
@@ -29,27 +32,27 @@ module.exports = {
         {
             resolve: `gatsby-source-strapi`,
             options: {
-                apiURL: process.env.API_URL || "http://localhost:1337",
+                apiURL: process.env.API_URL,
                 contentTypes: [`jobs`, `projects`, `blogs`],
                 singleTypes: [`about`],
                 queryLimit: 1000,
             },
         },
-        // {
-        //     resolve: `gatsby-plugin-prefetch-google-fonts`,
-        //     options: {
-        //         fonts: [
-        //             {
-        //                 family: `PT Serif`,
-        //                 variants: [`200`, `400`, `700`],
-        //             },
-        //             {
-        //                 family: `Mulish`,
-        //                 variants: [`400`, `700`],
-        //             },
-        //         ],
-        //     },
-        // },
+        {
+            resolve: `gatsby-plugin-prefetch-google-fonts`,
+            options: {
+                fonts: [
+                    {
+                        family: `PT Serif`,
+                        variants: [`200`, `400`, `700`],
+                    },
+                    {
+                        family: `Mulish`,
+                        variants: [`400`, `700`],
+                    },
+                ],
+            },
+        },
         {
             resolve: `gatsby-plugin-offline`,
             options: {
