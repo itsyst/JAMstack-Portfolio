423d87ca7736d5a80143ad59efe50740d5f11be2
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-10-16 13:19:27 +0200

    Updated services

--- portfolio_frontend/src/components/Services.js
+++ portfolio_frontend/src/components/Services.js
@@ -1,27 +1,127 @@
 import React from "react"
 import Title from "./Title"
 import services from "../constants/services"
+import styled, { css } from "styled-components"
+
 const Services = () => {
     return (
-        <section className="section bg-grey">
-            <Title title="Tjänster" />
-            <div className="section-center services-center">
+        <StyledServiceSection bgGrey>
+            <Title title="Kunskaper" />
+            <StyledServiceCenter servicesCenter>
                 {services.map(service => {
                     //  destructor
                     const { id, icon, title, text } = service
 
                     return (
-                        <article key={id} className="service">
+                        <StyledServiceArticle key={id}>
                             {icon}
                             <h4>{title}</h4>
-                            <div className="underline"></div>
+                            <StyledServiceUnderline />
                             <p>{text}</p>
-                        </article>
+                        </StyledServiceArticle>
                     )
                 })}
-            </div>
-        </section>
+            </StyledServiceCenter>
+        </StyledServiceSection>
     )
 }
 
+const StyledServiceSection = styled.section`
+    padding: 5rem 0;
+    ${props => props.bgGrey && css`
+        background: ${props => props.theme.colors.grey10};
+    `}
+`
+const StyledServiceCenter = styled.div`
+    width: 90vw;
+    margin: 0 auto;
+    max-width: 1170px;
+    @media screen and (min-width: 992px) {
+        width: 95vw;
+    }
+    ${props => props.servicesCenter && css`
+        @media screen and (min-width: 676px) {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            -webkit-column-gap: 2rem;
+            -moz-column-gap: 2rem;
+            column-gap: 2rem;
+        }
+        @media screen and (min-width: 992px) {
+            grid-template-columns: 1fr 1fr 1fr;
+        }
+    `}
+`
+const StyledServiceArticle = styled.article`
+    background: ${props => props.theme.colors.white};
+    padding: 3rem 1.5rem;
+    margin-bottom: 2rem;
+    border-radius: ${props => props.theme.radius.small};
+    text-align: center;
+    -webkit-transition: ${props => props.theme.animations.transition};
+    transition:  ${props => props.theme.animations.transition};
+    h4 {
+        letter-spacing: ${props => props.theme.spacings.xxSmall};
+        text-transform: capitalize;
+        line-height: 1.25;
+        margin-bottom: 0.75rem;
+        font-family: ${props => props.theme.fonts.primary};
+        font-size: 0.875rem;
+        color: ${props => props.theme.colors.black};
+
+        @media screen and (min-width: 800px) {
+            font-size: 1rem;
+            line-height: 1;
+        }
+    }
+    p {
+        -webkit-transition:  ${props => props.theme.animations.transition};
+        transition:  ${props => props.theme.animations.transition};
+        margin-bottom: 1.25rem;
+        color: ${props => props.theme.colors.grey3};
+    }
+
+
+    :hover {
+        background:  ${props => props.theme.colors.primary5};
+        box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.2); 
+        animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+        transform: translate3d(0, 0, 0);
+        backface-visibility: hidden;
+       
+        h4,p{
+            color: ${props => props.theme.colors.white};
+        } 
+        div {         
+            background: ${props => props.theme.colors.black};   
+        }
+
+                @keyframes shake {
+            10%, 90% {
+                transform: translate3d(-1px, 0, 0);
+            }
+            20%, 80% {
+                transform: translate3d(2px, 0, 0);
+            }
+
+            30%, 50%, 70% {
+                transform: translate3d(-4px, 0, 0);
+            }
+
+            40%, 60% {
+                transform: translate3d(4px, 0, 0);
+            }
+        }
+    } 
+
+`
+const StyledServiceUnderline = styled.div`
+    width: 3rem;
+    height: 0.12rem;
+    margin-bottom: 1.25rem;
+    background:${props => props.theme.colors.primary5};
+    margin-left: auto;
+    margin-right: auto;
+
+`
 export default Services
--- portfolio_frontend/src/constants/services.js
+++ portfolio_frontend/src/constants/services.js
@@ -3,24 +3,30 @@ import { FaCode } from "react-icons/fa"
 import { SiAdobexd } from "react-icons/si"
 import { VscEditorLayout } from "react-icons/vsc"
 
+const StyledIcon = {
+    fontSize: '2rem',
+    marginBottom: '1.5rem',
+}
 
 export default [
     {
         id: 1,
-        icon: <FaCode className="service-icon" />,
+        icon: <FaCode style={StyledIcon} />,
         title: "web development",
         text: `Jag kan skappa en dynamisk webbplats som går smidigt med moderna programmeringstekniker. (Laravel, Gatsby, VueJs, React, ASP.NET Core)`,
     },
     {
         id: 2,
-        icon: <VscEditorLayout className="service-icon" />,
+        icon: <VscEditorLayout style={StyledIcon} />,
         title: "Responsive Design",
         text: `Jag kan anpassa din webbplats till olika skärmstorlekar: Alla webbläsare och skärmar, mobiler, surfplattor, PC etc ...`,
     },
     {
         id: 3,
-        icon: <SiAdobexd className="service-icon" />,
+        icon: <SiAdobexd style={StyledIcon} />,
         title: "Web - app design",
         text: `Jag är redo att göra färgglada affischer, logotyper och kvalificerade webbdesigner med attraktiva effekter som fångar besökarens öga. `,
     },
 ]
+
+
