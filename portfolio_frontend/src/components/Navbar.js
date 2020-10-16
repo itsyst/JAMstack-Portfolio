c228651441e8da7880732c05de80e5d3bb01630c
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-10-16 13:17:58 +0200

    Created new nabar component

--- portfolio_frontend/src/components/Navbar.js
+++ portfolio_frontend/src/components/Navbar.js
@@ -1,21 +1,107 @@
-import React from "react"
-import logo from "../assets/logo.svg"
-import { FaAlignRight } from "react-icons/fa"
-import PageLinks from "../constants/links"
-const Navbar = ({ toggleSidebar }) => {
+import React, { useState } from "react"
+import Logo from "./Logo"
+import NavbarLinks from "../constants/links"
+import styled from "styled-components"
+
+const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false)
     return (
-        <nav className="navbar">
-            <div id="top" className="nav-center">
-                <div className="nav-header">
-                    <img src={logo} alt="logo" />
-                    <button type="button" className="toggle-btn" onClick={toggleSidebar}>
-                        <FaAlignRight></FaAlignRight>
-                    </button>
-                </div>
-                <PageLinks styleClass="nav-links"></PageLinks>
-            </div>
-        </nav>
+        <StyledNavbar>
+            <Logo />
+            <StyledToggleBtn
+                navbarOpen={navbarOpen}
+                onClick={() => setNavbarOpen(!navbarOpen)}>
+                {navbarOpen
+                    ? <StyledHamburger open />
+                    : <StyledHamburger />
+                }
+            </StyledToggleBtn>
+            {navbarOpen
+                ? (<StyledNavHeader><NavbarLinks /></StyledNavHeader>)
+                : (<StyledNavHeader open><NavbarLinks /></StyledNavHeader>
+                )}
+        </StyledNavbar>
     )
 }
 
+const StyledNavbar = styled.nav`
+  height: 10vh;
+  display: flex;
+  background-color: #fff;
+  position: relative;
+  justify-content: space-between;
+  text-transform: uppercase;
+  border-bottom: 2px solid #33333320;
+  margin: 0 auto;
+  padding: 0 5vw;
+  z-index: 2;
+  align-self: center;
+  @media (max-width: 768px) {
+    position: sticky;
+    height: 8vh;
+    top: 0;
+    left: 0;
+    right: 0;
+    left: 0;
+  }
+`
+const StyledHamburger = styled.div`
+  background-color: #111;
+  width: 30px;
+  height: 3px;
+  transition: all 0.3s linear;
+  align-self: center;
+  position: relative;
+  transform: ${props => (props.open ? "rotate(-45deg)" : "inherit")};
+
+  ::before,
+  ::after {
+    width: 30px;
+    height: 3px;
+    background-color: #111;
+    content: "";
+    position: absolute;
+    transition: all 0.3s linear;
+  }
+
+  ::before {
+    transform: ${props =>
+        props.open ? "rotate(-90deg) translate(-10px, 0px)" : "rotate(0deg)"};
+    top: -10px;
+  }
+
+  ::after {
+    opacity: ${props => (props.open ? "0" : "1")};
+    transform: ${props => (props.open ? "rotate(90deg) " : "rotate(0deg)")};
+    top: 10px;
+  }
+`
+const StyledNavHeader = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    position: fixed;
+    width: 100%;
+    justify-content: flex-start;
+    padding-top: 10vh;
+    background-color: #fff;
+    transition: all 0.3s ease-in;
+    top: 8vh;
+    left: ${props => (props.open ? "-100%" : "0")};
+  }
+`
+const StyledToggleBtn = styled.div`
+  display: none;
+  height: 100%;
+  cursor: pointer;
+  padding: 0 10vw;
+
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`
 export default Navbar
--- portfolio_frontend/src/constants/links.js
+++ portfolio_frontend/src/constants/links.js
@@ -1,43 +1,81 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 const data = [
-    {
-        id: 1,
-        text: "hem",
-        url: "/",
-    },
-    {
-        id: 4,
-        text: "Mitt CV",
-        url: "/about/",
-    },
-    {
-        id: 3,
-        text: "projekt",
-        url: "/projects/",
-    },
-    {
-        id: 4,
-        text: "blog",
-        url: "/blog/",
-    },
-    {
-        id: 5,
-        text: "kontakt",
-        url: "/contact/",
-    },
+  {
+    id: 1,
+    text: "hem",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "Mitt CV",
+    url: "/about/",
+  },
+  {
+    id: 3,
+    text: "projekt",
+    url: "/projects/",
+  },
+  {
+    id: 4,
+    text: "blog",
+    url: "/blog/",
+  },
+  {
+    id: 5,
+    text: "kontakt",
+    url: "/contact/",
+  },
 ]
 
 
-export default ({ styleClass }) => {
-    return (
-        <ul className={`page-links ${styleClass ? styleClass : ""}`}>
-            {data.map(link => (
-                <li key={link.id}>
-                    <Link to={link.url}>{link.text}</Link>
-                </li>
-            ))}
-        </ul>
-    )
+const NavbarLinks = () => {
+  return (
+    <>
+      {data.map(link => (
+        <StyledNavItems to={link.url}>{link.text}</StyledNavItems>
+      ))}
+    </>
+  )
 }
+
+const StyledNavItems = styled(Link)`
+  z-index:999;
+  text-decoration: none;
+  color: ${props => props.theme.colors.grey1};
+  display: inline-block;
+  white-space: nowrap;
+  margin: 0 1vw;
+  transition: all 200ms ease-in;
+  position: relative;
+
+  :after {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 0%;
+    content: ".";
+    color: transparent;
+    background: #33CCCC;
+    height: 1px;
+    transition: all 0.4s ease-in;
+  }
+
+  :hover {
+    color: #33CCCC;
+    ::after {
+      width: 100%;
+    }
+  }
+
+  @media (max-width: 768px) {
+    padding: 20px 0;
+    font-size: 1.5rem;
+    z-index: 6;
+  }
+
+`
+export default NavbarLinks
