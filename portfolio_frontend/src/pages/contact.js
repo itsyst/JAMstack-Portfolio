3d58b27af06ec10b92221dd08574d882f6f4c948
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-10-16 13:20:59 +0200

    Create a new contact page and added recaptcha

--- portfolio_frontend/src/pages/contact.js
+++ portfolio_frontend/src/pages/contact.js
@@ -3,12 +3,18 @@ import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import { navigate } from 'gatsby'
 import Recaptcha from 'react-google-recaptcha'
+import styled from "styled-components"
 
+const StyledRecaptcha = {
+    padding: '0 1.5rem',
+    textAlign: 'center',
+}
 
 const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
 if (typeof RECAPTCHA_KEY === 'undefined') {
     throw new Error("You probably forget to set it in your variables.")
 }
+
 
 function encode(data) {
     return Object.keys(data)
@@ -44,8 +50,8 @@ export default function Contact() {
     return (
         <Layout>
             <SEO title="Kontakt" description="Kontaktsidan" />
-            <section className="contact-page">
-                <article className="contact-form">
+            <StyledContactPage>
+                <StyledContactForm>
                     <h3>kontakta mig</h3>
                     <form
                         name="contact-recaptcha"
@@ -58,39 +64,143 @@ export default function Contact() {
                         <noscript>
                             <p>This form won’t work with Javascript disabled</p>
                         </noscript>
-                        <div className="form-group">
-                            <input
+                        <StyledFormGroup>
+                            <StyledFormControl
                                 type="text"
                                 placeholder="namn"
                                 name="name"
-                                className="form-control"
                                 required
                                 onChange={handleChange}
                             />
-                            <input
+                            <StyledFormControl
                                 type="email"
                                 placeholder="e-post"
                                 name="email"
-                                className="form-control"
                                 required
                                 onChange={handleChange}
                             />
-                            <textarea
+                            <StyledFormControlArea
                                 name="message"
                                 rows="5"
                                 placeholder="meddelande"
-                                className="form-control"
                                 required
                                 onChange={handleChange}
-                            ></textarea>
-                        </div>
-                        <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} className="form-recapcha" />
-                        <button type="submit" className="submit-btn btn">
+                            ></StyledFormControlArea>
+                        </StyledFormGroup>
+                        <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} style={StyledRecaptcha} />
+                        <StyledSubmitBtn type="submit">
                             Kontakta mig
-                    </button>
+                        </StyledSubmitBtn>
                     </form>
-                </article>
-            </section>
+                </StyledContactForm>
+            </StyledContactPage>
         </Layout>
     )
-}                       
+}
+
+const StyledContactPage = styled.section`
+    background: ${props => props.theme.colors.grey10};
+    min-height: calc(100vh - 5rem - 9rem);
+    display: grid;
+    place-items: center;
+    padding: 5rem 0;
+`
+const StyledContactForm = styled.article`
+    background:  ${props => props.theme.colors.white};
+    border-radius: ${props => props.theme.radius.small};
+    text-align: center;
+    box-shadow: ${props => props.theme.shadows.lightShadow};
+    transition:  ${props => props.theme.animations.transition};
+    width: 90vw;
+    max-width: 35rem;
+    :hover {
+        box-shadow: ${props => props.theme.shadows.darkShadow};
+    }
+
+    h3 {
+        letter-spacing: ${props => props.theme.spacings.small};
+        text-transform: capitalize;
+        line-height: 1.25;
+        margin-bottom: 0.75rem;
+        font-family: ${props => props.theme.colors.primary};
+        padding-top: 1.25rem;
+        color: ${props => props.theme.colors.grey5};
+        font-size: 1.25rem;
+
+        @media screen and (min-width: 800px) {
+            font-size: 1.25rem;
+            line-height: 1;
+        }
+    }
+`
+const StyledFormGroup = styled.div`
+    padding: 1rem 1.5rem;  
+`
+const StyledFormControl = styled.input`
+    display: block;
+    width: 100%;
+    padding: 0.75rem 1rem;
+    border: none;
+    margin-bottom: 1.25rem;
+    background:  ${props => props.theme.colors.grey10};
+    border-radius:  ${props => props.theme.radius.small};
+    text-transform: uppercase;
+    letter-spacing:  ${props => props.theme.spacings.xxSmall};
+
+    ::placeholder {
+        font-family: ${props => props.theme.fonts.primary};
+        color: ${props => props.theme.colors.grey1};
+        text-transform: uppercase;
+        letter-spacing: ${props => props.theme.spacings.xxSmall};
+    }
+`
+
+const StyledFormControlArea = styled.textarea`
+    display: block;
+    width: 100%;
+    padding: 0.75rem 1rem;
+    border: none;
+    margin-bottom: 1.25rem;
+    background:  ${props => props.theme.colors.grey10};
+    border-radius:  ${props => props.theme.radius.small};
+    text-transform: uppercase;
+    letter-spacing:  ${props => props.theme.spacings.xxSmall};
+
+    ::placeholder {
+        font-family: ${props => props.theme.fonts.primary};
+        color: ${props => props.theme.colors.grey1};
+        text-transform: uppercase;
+        letter-spacing: ${props => props.theme.spacings.xxSmall};
+    }
+`
+
+const StyledSubmitBtn = styled.button`
+    width: 100%;
+    border-bottom-left-radius: ${props => props.theme.radius.small};
+    border-bottom-right-radius: ${props => props.theme.radius.small};
+    border-top-right-radius: 0;
+    border-top-left-radius: 0;
+
+    text-transform: uppercase;
+    background: ${props => props.theme.colors.primary5};
+    color: ${props => props.theme.colors.primary9};
+    padding: 0.375rem 0.75rem;
+    letter-spacing: ${props => props.theme.spacings.xxSmall};
+    display: inline-block;
+    font-weight: 700;
+    -webkit-transition: ${props => props.theme.animations.transition};
+    transition: ${props => props.theme.animations.transition};
+    font-size: 0.875rem;
+    border: 2px solid transparent;
+    cursor: pointer;
+    box-shadow: ${props => props.theme.shadows.darkShadow};
+    border-radius:${props => props.theme.radius.small};
+    text-decoration: none;
+    padding: 0.6rem 1.2rem;
+
+    :hover {
+        color: ${props => props.theme.colors.primary1};
+        background: ${props => props.theme.colors.primary8};
+        box-shadow: ${props => props.theme.shadows.darkShadow};
+    }
+`
